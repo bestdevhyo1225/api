943da180679e5d2fb8bc527e76a1757a344c7da5
janghyoseog <bestdevhyo1225@gmail.com>
2020-12-25 15:40:53 +0900

    test: 테스트 코드 추가

--- src/test/java/com/hyoseok/dynamicdatasource/domain/item/entity/BookRepositoryTest.java
+++ src/test/java/com/hyoseok/dynamicdatasource/domain/item/entity/BookRepositoryTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.hyoseok.dynamicdatasource.domain.item.entity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+@SpringBootTest
+@Transactional
+@DisplayName("BookRepository 테스트")
 class BookRepositoryTest {
-  
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Autowired
+    private BookRepository repository;
+
+    @BeforeEach
+    void init() {
+        String title = "JPA";
+        String author = "author1";
+        int price = 25000;
+        String contents = "JPA 책이고, 반드시 공부 해야합니다.";
+
+        BookDescription bookDescription = BookDescription.builder().contents(contents).build();
+        Book book = Book.create(title, author, price, bookDescription, new ArrayList<>());
+
+        entityManager.persist(book);
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    @Test
+    void Book_Entity를_조회한다() {
+        Long bookId = 1L;
+        repository.findById(bookId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않음"));
+    }
 }
