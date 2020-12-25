db8494f7874d74083ecb53013389a946178cfe53
janghyoseog <bestdevhyo1225@gmail.com>
2020-12-25 15:42:11 +0900

    fix: OneToOne 양방향 관계이기 때문에 코드 수정

--- src/main/java/com/hyoseok/dynamicdatasource/domain/item/entity/BookDescription.java
+++ src/main/java/com/hyoseok/dynamicdatasource/domain/item/entity/BookDescription.java
@@ -14,15 +14,21 @@ import javax.persistence.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookDescription {
 
+    /*
+    * Book 엔티티의 Id가 BookDescription 엔티티의 Id에 자동으로 매핑되기 때문에 @GeneratedValue를 사용할 필요가 없음
+    * */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "book_description_id")
-    private Long id;
+    private Long bookId;
 
     @Column(nullable = false, length = 500)
     private String contents;
 
-    @MapsId // 'BookDescription.id'와 매핑된다. 외래 키와 매핑한 연관관계를 기본 키에도 매핑하겠다는 뜻이다.
+    /*
+    * @MapsId
+    * 외래 키와 매핑한 연관관계를 기본 키에도 매핑하겠다는 뜻이다.
+    * 즉, BookDescription 엔티티의 Id와 Book 엔티티의 Id를 매핑시킨다는 의미!
+    * */
+    @MapsId
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
