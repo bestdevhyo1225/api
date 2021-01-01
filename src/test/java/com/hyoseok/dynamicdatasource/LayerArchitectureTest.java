2d6d232af3f3b8e956a4ba4c95f1e5eda8a18d4f
janghyoseog <bestdevhyo1225@gmail.com>
2021-01-02 07:38:20 +0900

    test: 레이어 아키텍쳐 테스트 추가

---
+++ src/test/java/com/hyoseok/dynamicdatasource/LayerArchitectureTest.java
@@ -0,0 +1,21 @@
+package com.hyoseok.dynamicdatasource;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.library.Architectures.*;
+
+@AnalyzeClasses(packages = "com.hyoseok.dynamicdatasource")
+public class LayerArchitectureTest {
+
+    @ArchTest
+    static final ArchRule 레이어_의존성을_테스트한다 = layeredArchitecture()
+            .layer("Controller").definedBy("com.hyoseok.dynamicdatasource.web")
+            .layer("Service").definedBy("com.hyoseok.dynamicdatasource.domain.usecase.*")
+            .layer("Repository").definedBy("com.hyoseok.dynamicdatasource.domain.entity.*")
+            .layer("Data").definedBy("com.hyoseok.dynamicdatasource.data")
+            .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
+            .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service", "Data");
+}
