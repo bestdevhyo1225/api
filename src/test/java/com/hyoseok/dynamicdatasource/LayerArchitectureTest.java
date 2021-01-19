97b14698267a8f938377de9805d53974bfd64ed5
janghyoseog <bestdevhyo1225@gmail.com>
2021-01-19 12:05:10 +0900

    test: LayerArchitectureTest 수정

--- src/test/java/com/hyoseok/dynamicdatasource/LayerArchitectureTest.java
+++ src/test/java/com/hyoseok/dynamicdatasource/LayerArchitectureTest.java
@@ -11,11 +11,11 @@ public class LayerArchitectureTest {
 
     @ArchTest
     static final ArchRule 레이어_의존성을_테스트한다 = layeredArchitecture()
-            .layer("Controller").definedBy("com.hyoseok.dynamicdatasource.web.http")
-            .layer("Service").definedBy("com.hyoseok.dynamicdatasource.usecase.*")
-            .layer("Repository").definedBy("com.hyoseok.dynamicdatasource.domain.*")
+            .layer("Web").definedBy("com.hyoseok.dynamicdatasource.web.http")
+            .layer("Usecase").definedBy("com.hyoseok.dynamicdatasource.usecase.*")
+            .layer("Domain").definedBy("com.hyoseok.dynamicdatasource.domain.*")
             .layer("Data").definedBy("com.hyoseok.dynamicdatasource.data")
-            .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
-            .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
-            .whereLayer("Repository").mayOnlyBeAccessedByLayers("Service", "Data");
+            .whereLayer("Web").mayNotBeAccessedByAnyLayer()
+            .whereLayer("Usecase").mayOnlyBeAccessedByLayers("Web")
+            .whereLayer("Domain").mayOnlyBeAccessedByLayers("Usecase", "Data");
 }
